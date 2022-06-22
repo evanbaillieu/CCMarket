1d8aa2f0301c9aec8333889373aa78db1cfa0504
Simon <zou-simon@hotmail.fr>
2022-06-22 10:57:16 +0200

    feat(front): fetch data on account pages

--- assets/js/view/Account/ChangePassword.tsx
+++ assets/js/view/Account/ChangePassword.tsx
@@ -1,40 +1,121 @@
-import React from 'react';
+import React, { ChangeEvent, FC, useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
+import Inpute from '../../components/input';
+import { checkIsEmpty, checkIsNotEmpty, getTokken } from '../../helper/utilHelper';
+import useForm from '../../hook/useForm';
+import { getMe, updatePassword } from '../../service/accountService';
+import Oeil from '../../svg/oeil.svg';
+import OeilFermer from '../../svg/oeilFermer.svg';
 
-export default function ChangePassword() {
+const ChangePassword: FC = () => {
     const { t } = useTranslation();
+    const [isVisibleCurrentPass, setIsVisibleCurrentPass] = useState(false);
+    const [isVisibleNewPass, setIsVisibleNewPass] = useState(false);
+    const [isVisibleConfirmNewPass, setIsVisibleConfirmNewPass] = useState(false);
+
+    const { data, errors, hangleChange } = useForm({
+        password: '',
+    });
+
+    const [userId, setUserId] = useState<string>();
+
+    useEffect(() => {
+        getMe(getTokken()).then((response) => {
+            setUserId(response.user.id);
+        });
+    }, []);
+
+    const [confirmNewPassword, setConfirmNewPassword] = useState<string>('');
+    const [currentPassword, setCurrentPassword] = useState<string>('');
+
+    const handleChangePWD = (e: ChangeEvent<HTMLInputElement>) => {
+        setConfirmNewPassword(e.target.value);
+    };
+
+    const handleChangeCurrentPWD = (e: ChangeEvent<HTMLInputElement>) => {
+        setCurrentPassword(e.target.value);
+    };
+
+    const submit = () => {
+        if (checkIsEmpty(data) && !checkIsNotEmpty(errors)) {
+            return;
+        }
+
+        if (confirmNewPassword === data.password) {
+            console.log('se son les meme');
+        }
+
+        updatePassword(getTokken(), userId, data.password);
+    };
+
+    const changeVisbleCurrentPass = () => {
+        setIsVisibleCurrentPass((current) => !current);
+    };
+
+    const changeVisbleNewPass = () => {
+        setIsVisibleNewPass((current) => !current);
+    };
+
+    const changeVisbleConfirmNewPass = () => {
+        setIsVisibleConfirmNewPass((current) => !current);
+    };
+
     return (
-        <form className="edit-form">
+        <div className="edit-form">
             <h1>{t('account.changePassword')}</h1>
             <div id="change-password-inputs">
-                <div className="grid-content">
-                    <label htmlFor="currentPassword" className="h6 required">
-                        {t('account.currentPassword')}
-                    </label>
-                    <input type="password" name="currentPassword" id="currentPassword" />
-                </div>
-                <div className="grid-content">
-                    <label htmlFor="newPassword" className="h6 required">
-                        {t('account.newPassword')}
-                    </label>
-                    <input type="password" name="newPassword" id="newPassword" />
-                </div>
-                <div className="grid-content">
-                    <label htmlFor="confirmNewPassword" className="h6 required">
-                        {t('account.confirmNewPassword')}
-                    </label>
-                    <input type="password" name="confirmNewPassword" id="confirmNewPassword" />
-                </div>
+                <Inpute
+                    svg={isVisibleCurrentPass ? <Oeil /> : <OeilFermer />}
+                    option={{
+                        type: isVisibleCurrentPass ? 'text' : 'password',
+                        name: 'currentPassword',
+                        title: `account.currentPassword`,
+                        error: '',
+                        validate: '',
+                    }}
+                    handleChange={handleChangeCurrentPWD}
+                    value={currentPassword}
+                    onClick={changeVisbleCurrentPass}
+                />
+
+                <Inpute
+                    svg={isVisibleNewPass ? <Oeil /> : <OeilFermer />}
+                    option={{
+                        type: isVisibleNewPass ? 'text' : 'password',
+                        name: 'password',
+                        title: `account.newPassword`,
+                        error: errors.password,
+                        validate: '',
+                    }}
+                    handleChange={hangleChange}
+                    value={data.password}
+                    onClick={changeVisbleNewPass}
+                />
+
+                <Inpute
+                    svg={isVisibleConfirmNewPass ? <Oeil /> : <OeilFermer />}
+                    option={{
+                        type: isVisibleConfirmNewPass ? 'text' : 'password',
+                        name: 'confirmNewPassword',
+                        title: `account.confirmNewPassword`,
+                        error: '',
+                        validate: '',
+                    }}
+                    handleChange={handleChangePWD}
+                    value={confirmNewPassword}
+                    onClick={changeVisbleConfirmNewPass}
+                />
             </div>
             <div className="edit-buttons">
                 <Link to={'/account'} className="btn btn-cancel">
                     {t('account.cancel')}
                 </Link>
-                <button type="submit" className="btn btn-primary">
+                <button className="btn btn-primary" onClick={submit}>
                     {t('account.save')}
                 </button>
             </div>
-        </form>
+        </div>
     );
-}
+};
+export default ChangePassword;
--- assets/js/view/Account/index.tsx
+++ assets/js/view/Account/index.tsx
@@ -1,24 +1,42 @@
-import React, { useState } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Link, useOutlet } from 'react-router-dom';
+import { getMe } from '../../service/accountService';
+import { getTokken } from '../../helper/utilHelper';
 import profile from '../../img/test.png';
+import { IUser } from '../../constant/Type/entity';
+import { useAppDispatch } from '../../store/store';
+import { logout } from '../../store/reducer/settingReducer';
 
-export default function Account() {
+const Account: FC = () => {
     const { t } = useTranslation();
-    const outlet = useOutlet();
-
-    const [user, setUser] = useState({
-        firstName: 'John',
-        lastName: 'Doe',
+    const [user, setUser] = useState<IUser>({
+        firstName: '',
+        lastName: '',
+        email: '',
+        dateOfBirth: '',
     });
+    const outlet = useOutlet(user);
+    const AppDispatch = useAppDispatch();
+
+    useEffect(() => {
+        getMe(getTokken()).then((response) => {
+            setUser({ ...response.user });
+        });
+    }, []);
 
     const getNavLinkClass = (path: string) => {
         return window.location.pathname === path ? 'active' : '';
     };
 
+    const accountLogout = () => {
+        AppDispatch(logout());
+        window.localStorage.removeItem('auth-cmarket');
+    };
+
     return (
-        <div id="account">
-            <div className="sidebar">
+        <div id="account-container">
+            <div id="account-sidebar">
                 <img src={profile} width={150} height={150} alt="Profile" />
                 <h3>{user.firstName + ' ' + user.lastName}</h3>
                 <ul>
@@ -33,11 +51,14 @@ export default function Account() {
                         </Link>
                     </li>
                     <li>
-                        <Link to={'/logout'}>{t('account.logout')}</Link>
+                        <Link to={'/'} onClick={accountLogout}>
+                            {t('account.logout')}
+                        </Link>
                     </li>
                 </ul>
             </div>
             {outlet}
         </div>
     );
-}
+};
+export default Account;
